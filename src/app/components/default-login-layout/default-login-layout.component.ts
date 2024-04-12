bce75a586fbe764df4676d9c5b4a34f17675429f
Luiz Henrique Caldas <luizhpcaldas@gmail.com>
2024-04-12 12:01:52 -0300

    css sections

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-default-login-layout',
@@ -10,5 +10,7 @@ import { Component } from '@angular/core';
   styleUrl: './default-login-layout.component.scss'
 })
 export class DefaultLoginLayoutComponent {
-
+  @Input() title: string = "";
+  @Input() primaryBtnText: string = "";
+  @Input() secondaryBtnText: string="";
 }
