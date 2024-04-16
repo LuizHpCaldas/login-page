15a1fee907b4fefb5de535299d371f3123c00b2e
Luiz Henrique Caldas <luizhpcaldas@gmail.com>
2024-04-16 11:22:51 -0300

    feat: create login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,6 +3,9 @@ import { DefaultLoginLayoutComponent } from '../../components/default-login-layo
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-login',
@@ -12,13 +15,18 @@ import { Router } from '@angular/router';
     ReactiveFormsModule,
     PrimaryInputComponent
   ],
+  providers: [
+    LoginService
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   loginForm!: FormGroup;
   constructor(
-    private router: Router
+    private router: Router,
+    private loginService: LoginService,
+    private toastService: ToastrService
   ){
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
@@ -27,7 +35,10 @@ export class LoginComponent {
   }
 
   submit(){
-    console.log(this.loginForm.value)
+    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: () => this.toastService.success("Login efetuado com sucesso!"),
+      error: () => this.toastService.error("Erro inesperado, tente novamente mais tarde!")
+    })             
   }
 
   navigate(){
